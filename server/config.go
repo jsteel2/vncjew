5c7e779660cb3ccb8970c2a44dd783905590bf5a
Your Name <you@example.com>
2023-04-18 06:30:14 +0200

    Let admin do client things because well hes admingit status

--- server/config.go
+++ server/config.go
@@ -2,8 +2,13 @@ package main
 
 import "time"
 
-var CFGAdminAccount = map[string]string{"admin": "***REMOVED***"}
-var CFGClientAccount = map[string]string{"client": "***REMOVED***"}
+var CFGAdminAccount = map[string]string{
+	"admin": "***REMOVED***",
+}
+var CFGClientAccount = map[string]string{
+	"client": "***REMOVED***",
+	"admin": CFGAdminAccount["admin"],
+}
 var CFGMaxVNCConns = 40
 var CFGMaxConcurrentOCR = 1
 var CFGDb = "database.sqlite3"
